438fc3fac411936ec86cdca1f34300a3761c3ff6
Michael Kelly <mike.s.kelly@icloud.com>
2019-04-09 15:10:03 -0700

    Add project files.

---
+++ VirtualRPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirtualRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
